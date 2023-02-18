bd18dd04538246b9de47b38f7b9a174756d58f5b
bimaikhsan <bimaikhsan.bi@gmail.com>
2023-02-18 19:23:47 +0700

    fix

--- resources/js/components/EditPost.vue
+++ resources/js/components/EditPost.vue
@@ -95,39 +95,50 @@
                                 Tambah Rincian
                             </button> -->
                             <div v-for="(data, index) in datauraian" :key="index">
-                                <div class="flex justify-start ml-2 mt-4">
-                                    <VueMultiselect
-                                    placeholder="Masukan Nama Uraian"
-                                    v-model="data.nama"
-                                    :options="optionsuraian"
-                                    mode="tags">
-                                        <template #noResult>
-                                        tidak ada data Uraian
-                                        </template>
-                                    </VueMultiselect>
-                                <!-- <input
-                                    v-model="data.nama"
-                                    placeholder="enter you course name"
-                                    class="w-full py-2 px-3 border border-indigo-500 rounded"
-                                /> -->
-                                <input
-                                    v-model="data.debet"
-                                    placeholder="Masukkan Debet"
-                                    class="w-full py-2 px-3 border border-indigo-500 rounded"
-                                />
-                                <input
-                                    v-model="data.kredit"
-                                    placeholder="Masukkan Kredit"
-                                    class="w-full py-2 px-3 border border-indigo-500 rounded"
-                                />
-                                <!-- <button
-                                    type="button"
-                                    class="ml-2 rounded-md border px-3 py-2 bg-red-600 text-white"
-                                    @click="remove(index)"
-                                    v-show="index != 0"
-                                >
-                                    Remove
-                                </button> -->
+                                <div class="row">
+                                    <div class="col p-3">
+                                        <div class="inpute- mb-3">
+                                            <label>Nama Uraian</label><span class="text-danger"> *</span>
+                                                <div class="input-group input-group-outline mb-3">
+                                                    <VueMultiselect
+                                                    placeholder="Masukan Nama Uraian"
+                                                    v-model="data.nama"
+                                                    :options="optionsuraian"
+                                                    mode="tags">
+                                                        <template #noResult>
+                                                        tidak ada data Uraian
+                                                        </template>
+                                                    </VueMultiselect>
+                                                </div>
+                                            </div>
+                                    </div>
+                                    <div class="col p-3">
+                                        <div class="inpute- mb-3">
+                                            <label>Debet</label>
+                                            <div class="input-group input-group-outline mb-3">
+                                                <input
+                                                    type="number"
+                                                    v-model="data.debet"
+                                                    placeholder="Masukkan Debet"
+                                                    class="form-control"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col p-3">
+                                        <div class="inpute- mb-3">
+                                            <label>Debet</label>
+                                            <div class="input-group input-group-outline mb-3">
+                                                <input
+                                                    type="number"
+                                                    v-model="data.kredit"
+                                                    placeholder="Masukkan Kredit"
+                                                    class="form-control"
+
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <button type="submit" class="btn btn-primary mt-4 mb-4"> Update</button>
